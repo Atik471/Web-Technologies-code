23b61b42ec6ed188d424300ed4f9b97fa92bde49
Atik471 <atikurr.contact@gmail.com>
2025-08-16 23:05:16 +0600

    updated code

--- JavaScript/form2.js
+++ JavaScript/form2.js
@@ -18,16 +18,3 @@ function submitScore(event) {
 	}
 
 }
-
-Create an HTML page with a square box (<div>) styled with position: absolute;.
-Also add a button labeled "Move Box".
-When the button is clicked, the box should move to a random position within the browser window.
-Use addEventListener to listen for the click event and generate random top and left values to reposition the box.
-
-Example:
-
-First click → Box moves to top-left area.
-
-Second click → Box jumps near the bottom-right.
-
-Third click → Box moves somewhere in the middle.
